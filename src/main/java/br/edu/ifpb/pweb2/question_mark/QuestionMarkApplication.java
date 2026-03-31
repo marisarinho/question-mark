0b1c9c83d07aff3444675ccb169686d61e45bea5
marisarinho <sarinho.mariana@academico.ifpb.edu.br>
2026-03-31 10:59:45 -0300

    start: esqueleto do projeto

---
+++ src/main/java/br/edu/ifpb/pweb2/question_mark/QuestionMarkApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.pweb2.question_mark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionMarkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionMarkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifpb/pweb2/question_mark/QuestionMarkApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.pweb2.question_mark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionMarkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
