35ff0287e686623bf940f47db36be22f34355dab
marisarinho <sarinho.mariana@academico.ifpb.edu.br>
2026-06-22 20:05:33 -0300

    refactor: define status das excecoes

--- src/main/java/br/edu/ifpb/pweb2/question_mark/exception/RecursoNaoEncontradoException.java
+++ src/main/java/br/edu/ifpb/pweb2/question_mark/exception/RecursoNaoEncontradoException.java
@@ -1,5 +1,9 @@
 package br.edu.ifpb.pweb2.question_mark.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class RecursoNaoEncontradoException extends RuntimeException {
 
     public RecursoNaoEncontradoException(String mensagem) {
